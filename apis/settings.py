cd92d5dca187113789375bc0ea088d48fc331b2e
Azuka <ajchiejina@gmail.com>
2022-05-02 00:42:04 +0100

    migrations

--- apis/settings.py
+++ apis/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'apis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apis',
+        'USER': 'postgres',
+        'PASSWORD': '590447',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -124,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "profiles_api.UserProfile"
