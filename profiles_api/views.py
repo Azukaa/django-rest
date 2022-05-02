c69db6cae9e43776a1e0e7a06a2b6d9af3f88b2c
Azuka <ajchiejina@gmail.com>
2022-05-02 21:43:05 +0100

    viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import HelloSerializers
+from rest_framework import viewsets
 # Create your views here.
 
 
@@ -26,7 +27,7 @@ class HelloApiView(APIView):
         return Response(context)
 
     def post(self, request):
-
+        """CREATE A NEW MESSAGE"""
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
@@ -42,6 +43,7 @@ class HelloApiView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk=None):
+        """UPDATE A MESSAGE"""
         context = {
             "message": "PUT request"
         }
@@ -57,4 +59,68 @@ class HelloApiView(APIView):
         context = {
             "message": "Delete request"
         }
+        return Response(context)
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = HelloSerializers
+
+    def list(self, request):
+
+        all_viewsets = [
+            "Uses actions such as list, create, update, retrieve",
+            "Automatically maps to URLs using routers",
+            "Provides more functionality with less code",
+        ]
+        context = {
+            "message": "This shows all we can do with viewsets",
+            "all_viewsets": all_viewsets,
+        }
+
+        return Response(context)
+
+    def create(self, request):
+        """CREATING A NEW MESSAGE"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}!"
+            context = {
+                "message": message
+            }
+            return Response(context)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        "HANDLE RETRIEVING AN OBJECT BY ID"
+        context = {
+            "http_method": "GET"
+        }
+
+        return Response(context)
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        context = {
+            "http_method": "PUT"
+        }
+
+        return Response(context)
+
+    def partial_update(self, request, pk=None):
+        """Handle updating a part of an object"""
+        context = {
+            "http_method": "PATCH"
+        }
+
+        return Response(context)
+
+    def destroy(self, request, pk=None):
+        """Handle Removing an object"""
+        context = {
+            "http_method": "Delete"
+        }
+
         return Response(context)
