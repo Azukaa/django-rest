6c5f5132c44b51514e4256ec102daa37db7fe404
Azuka <ajchiejina@gmail.com>
2022-05-05 15:47:02 +0100

    UPDATE

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/Azukaa/django-rest.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/django-profiles-rest-api'
 
 echo "Installing dependencies..."
 apt-get update
